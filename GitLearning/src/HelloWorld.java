3a6c0a36b50bb32d31a3fabc085ab1a9758faf3b
Falguni Hensh <iamfalgunihensh@gmail.com>
2022-08-07 10:59:06 +0530

    Commit updated

--- GitLearning/src/HelloWorld.java
+++ GitLearning/src/HelloWorld.java
@@ -4,6 +4,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!!");
+		
+		String[] names = {"Abhi", "Ajaya", "Sanghita", "Ankush", "Biplob"};
+		
+		for (int i = 0; i < names.length; i++) {
+			System.out.format("Hello  %s \n", names[i]);
+			
+		}
 	}
 
 }
